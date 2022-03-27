d6d6ed696e27381f3233ae9bb75c87f229ef6999
Mohammad Ali Khan <xvirus.bd@gmail.com>
2022-03-28 01:21:21 +0600

    advisor offer max 18 credit

--- app/offer-course.php
+++ app/offer-course.php
@@ -155,7 +155,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save'])) {
     for (var i=1;i<=numberOfChecked;i++) {
         totres += parseInt(number);
     }
-    if(totres >= 16){
+    if(totres >= 19){
             alert('You can not offer more than 15 credit');
             // $('.checkItem').attr('disabled', true);
             $('input:checkbox:not(":checked")').attr('disabled', true);
